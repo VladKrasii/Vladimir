5191625d6ee048443fe00aba3329fa3b6121f8ee
VladKrasii <59770952+VladKrasii@users.noreply.github.com>
2020-01-18 11:52:16 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+class Program
+{
+    static bool Divisibility(int 9)
+    {
+        if (9 % 3 == 3)
+        {
+            return true;
+            {
+                Console.WriteLine
+                {
+                    class Program
+                    {
+                        static bool Divisibility(int 25)
+                    }
+                    if (25 % 3 ==[8 * 3,3 ])
+                    {
+                        return false;
+                        {
+                            static void Main(string[] args)
+                            {
+                                class Program
+                                {
+                                    static bool Divisibility(int 102)
+                                    {
+                                        if (102 % 3==34)
+                                    }
+            return true;
+                                    {
+                                        Console.WriteLine
+                                        {
+
+                                        }
+
+        System.out.WriteLine
